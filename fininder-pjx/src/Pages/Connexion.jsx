3acb07c83410bf357577996c19966c820dfead38
kengerardoesteve <kengerardoesteve@gmail.com>
2026-07-10 15:03:16 +0200

    Organisation des composants et des pages

---
+++ fininder-pjx/src/Pages/Connexion.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Service from "./Pages/Service";
+import Accueil from "./Pages/Accueil";
+import Contact from "./Pages/Contact";
+import Connection from "./Pages/Connexion";
+import Inscription from "./Pages/Inscription";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Accueil />} />
+        <Route path="/service" element={<Service />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/connection" element={<Connection />} />
+        <Route path="/inscription" element={<Inscription />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
